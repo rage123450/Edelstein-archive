b5f1d6d8b4619c10cf4ccf16b93c98e43ef08ef7
Kaioru <keifuurawrs@gmail.com>
2018-09-25 21:24:54 +0800

    Add Rand32 referenced from client

---
+++ test/Edelstein.Common.Tests/Rand32Test.cs
@@ -0,0 +1,25 @@
+using System;
+using Xunit;
+
+namespace Edelstein.Common.Tests
+{
+    public class Rand32Test
+    {
+        [Fact]
+        public void RandomResultIsAccurate()
+        {
+            var rand = new Rand32(100, 200, 300);
+            
+            Assert.Equal(0x2464C80, rand.Random());
+        }
+        
+        [Fact]
+        public void PrevRandomResultIsAccurate()
+        {
+            var rand = new Rand32(100, 200, 300);
+            var prev = rand.Random();
+            
+            Assert.Equal(prev, rand.PrevRandom());
+        }
+    }
+}
