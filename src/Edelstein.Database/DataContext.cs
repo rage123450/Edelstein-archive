51ba11aa2cbab18784e16ab424d375fea05f65b5
Kaioru <keifuurawrs@gmail.com>
2018-09-26 14:38:56 +0800

    Add graphing to inventory items

--- src/Edelstein.Database/DataContext.cs
+++ src/Edelstein.Database/DataContext.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Edelstein.Database.Entities;
 using Edelstein.Database.Entities.Inventory;
@@ -30,6 +31,20 @@ namespace Edelstein.Database
                 var existing = Characters
                     .AsNoTracking()
                     .Include(c => c.FunctionKeys)
+                    .Include(c => c.InventoryEquipped)
+                    .ThenInclude(c => c.Items)
+                    .Include(c => c.InventoryEquippedCash)
+                    .ThenInclude(c => c.Items)
+                    .Include(c => c.InventoryEquip)
+                    .ThenInclude(c => c.Items)
+                    .Include(c => c.InventoryConsume)
+                    .ThenInclude(c => c.Items)
+                    .Include(c => c.InventoryInstall)
+                    .ThenInclude(c => c.Items)
+                    .Include(c => c.InventoryEtc)
+                    .ThenInclude(c => c.Items)
+                    .Include(c => c.InventoryCash)
+                    .ThenInclude(c => c.Items)
                     .FirstOrDefault(c => c.ID == character.ID);
 
                 if (existing != null)
@@ -38,6 +53,29 @@ namespace Edelstein.Database
                     {
                         if (character.FunctionKeys.All(f => f.ID == functionKey.ID))
                             Remove(functionKey);
+                    }
+
+                    var existingItems = new List<ItemSlot>();
+                    existingItems.AddRange(existing.InventoryEquipped.Items);
+                    existingItems.AddRange(existing.InventoryEquippedCash.Items);
+                    existingItems.AddRange(existing.InventoryEquip.Items);
+                    existingItems.AddRange(existing.InventoryConsume.Items);
+                    existingItems.AddRange(existing.InventoryInstall.Items);
+                    existingItems.AddRange(existing.InventoryEtc.Items);
+                    existingItems.AddRange(existing.InventoryCash.Items);
+                    var currentItems = new List<ItemSlot>();
+                    currentItems.AddRange(character.InventoryEquipped.Items);
+                    currentItems.AddRange(character.InventoryEquippedCash.Items);
+                    currentItems.AddRange(character.InventoryEquip.Items);
+                    currentItems.AddRange(character.InventoryConsume.Items);
+                    currentItems.AddRange(character.InventoryInstall.Items);
+                    currentItems.AddRange(character.InventoryEtc.Items);
+                    currentItems.AddRange(character.InventoryCash.Items);
+
+                    foreach (var existingItem in existingItems)
+                    {
+                        if (currentItems.All(f => f.ID == existingItem.ID))
+                            Remove(existingItem);
                     }
                 }
             }
