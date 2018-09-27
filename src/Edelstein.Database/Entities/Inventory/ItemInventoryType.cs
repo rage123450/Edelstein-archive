dc0f09bd7ee5f8f1981323b06a5034a195a147a1
Kaioru <keifuurawrs@gmail.com>
2018-09-27 23:14:22 +0800

    Change inventory type values

--- src/Edelstein.Database/Entities/Inventory/ItemInventoryType.cs
+++ src/Edelstein.Database/Entities/Inventory/ItemInventoryType.cs
@@ -2,15 +2,15 @@ namespace Edelstein.Database.Entities.Inventory
 {
     public enum ItemInventoryType
     {
-        Equip = 0x0,
-        Consume = 0x1,
-        Install = 0x2,
-        Etc = 0x3,
-        Cash = 0x4,
+        Equip = 0x1,
+        Consume = 0x2,
+        Install = 0x3,
+        Etc = 0x4,
+        Cash = 0x5,
 
-        Equipped = 0x5,
-        EquippedCash = 0x6,
-        EquippedDragon = 0x7,
-        EquippedMechanic = 0x8
+        Equipped = 0x6,
+        EquippedCash = 0x7,
+        EquippedDragon = 0x8,
+        EquippedMechanic = 0x9
     }
 }
