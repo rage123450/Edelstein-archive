af445edc9e111fb0aaed6870d183a47786c76623
Kaioru <keifuurawrs@gmail.com>
2018-10-02 20:31:06 +0800

    Uncomment debug buff command for testing

--- src/Edelstein.WvsGame/Commands/Impl/BuffCommand.cs
+++ src/Edelstein.WvsGame/Commands/Impl/BuffCommand.cs
@@ -16,11 +16,11 @@ namespace Edelstein.WvsGame.Commands.Impl
             {
                 var date = DateTime.Now;
                 s.Set(TemporaryStatType.BlessingArmor, 30001000, 10, date.AddSeconds(30));
-                /*s.Set(TemporaryStatType.PDD, 30001000, 10, date.AddSeconds(30));
+                s.Set(TemporaryStatType.PDD, 30001000, 10, date.AddSeconds(30));
                 s.Set(TemporaryStatType.MAD, 30001000, 10, date.AddSeconds(30));
                 s.Set(TemporaryStatType.MDD, 30001000, 10, date.AddSeconds(30));
                 s.Set(TemporaryStatType.Speed, 30001000, 100, date.AddSeconds(30));
-                s.Set(TemporaryStatType.Jump, 30001000, 100, date.AddSeconds(30));*/
+                s.Set(TemporaryStatType.Jump, 30001000, 100, date.AddSeconds(30));
             });
             ctx.User.Message("Successfully set temporary stats.");
             return Task.CompletedTask;
