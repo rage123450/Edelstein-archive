761069982f1e7def8bd3f572223a9c2b22bc1243
Kaioru <keifuurawrs@gmail.com>
2018-10-20 12:38:33 +0800

    Rename CIGCipher tests to IGCipher

--- test/Edelstein.Network.Tests/Crypto/IGCipherTest.cs
+++ test/Edelstein.Network.Tests/Crypto/IGCipherTest.cs
@@ -3,7 +3,7 @@ using Xunit;
 
 namespace Edelstein.Network.Tests.Crypto
 {
-    public class CIGCipherTest
+    public class IGCipherTest
     {
         [Fact]
         public void HashResultIsAccurate()
